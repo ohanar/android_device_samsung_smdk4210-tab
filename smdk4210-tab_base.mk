64e911aca6ffa07a273715fa180d7f9d91faa3ac
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-08-05 00:30:09 -0700

    add audio.low_latency permissions

--- smdk4210-tab_base.mk
+++ smdk4210-tab_base.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
